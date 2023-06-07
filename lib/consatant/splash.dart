4bcec9a12892dd75f00d4a93759a2a9812f42c51
Baaav23 <bavlybadry123@gmail.com>
2023-06-07 11:45:33 +0300

    Add files via upload

---
+++ lib/consatant/splash.dart
@@ -0,0 +1,22 @@
+import 'package:btxproject2/consatant/constant.dart';
+import 'package:btxproject2/pages/students_Affairs/Home/Main_Screen_Home.dart';
+import 'package:flutter/material.dart';
+import 'package:splash_view/source/presentation/presentation.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color1 = HexColor('#3E6BA9');
+    return SplashView(
+        backgroundColor: color1,
+        loadingIndicator: const RefreshProgressIndicator(),
+        logo: Image.asset(
+          "assets/img/image1.png",
+          width: 100,
+          height: 100,
+        ),
+        done: Done(const MainScreenHome()));
+  }
+}
